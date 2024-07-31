f7f331242aee881566d1a99177131ab1fb380a14
Jon Nyman <nymanjon@gmail.com>
2024-07-31 11:30:57 -0700

    Added snippet for cleaning up old local git branches

--- windows-snippets.ps1
+++ windows-snippets.ps1
@@ -4,3 +4,5 @@
 # Gracefully close all windows
 (Get-Process | ? { $_.MainWindowTitle -ne "" } ) | % { $_.CloseMainWindow() }
 
+# Delete all local git branches except for current and master branches
+git branch | ? { -not ($_.Trim().StartsWith("*") -or $_.Trim() -eq "master")  } | % { git branch -D $_.Trim() }
